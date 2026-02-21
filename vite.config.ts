15efa07cc7f46f7e4d01da5c57815a9a9c7e0fe7
Kirill <kirill_alekseev-96@mail.ru>
2026-02-21 11:13:25 +0300

    пустой коммит для триггера деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/2437017-six-cities-3/', // 👈 Добавлено для GitHub Pages
+  build: {
+    outDir: 'docs', // 👈 Будет собирать сразу в docs
+  },
   test: {
     globals: true,
     environment: 'jsdom',
